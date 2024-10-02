1c09cf16115ee3c69cc18660c90ac35d2ed5d7b8
Dinh Viet Long <long37k1999@gmail.com>
2024-10-02 15:55:11 +0700

    setup dependency connect data mysql

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -2,7 +2,7 @@ package vn.hoidanit.laptopshop;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
