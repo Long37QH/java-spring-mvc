908f6636d7469b81d47745c4e9b55c050fe009c6
Dinh Viet Long <long37k1999@gmail.com>
2024-10-04 08:46:20 +0700

    set up forder can thiet test controller

---
+++ src/main/java/vn/hoidanit/laptopshop/repository/UserService.java
@@ -0,0 +1,11 @@
+package vn.hoidanit.laptopshop.repository;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public String hello(){
+        return "hello from service";
+    }
+}
